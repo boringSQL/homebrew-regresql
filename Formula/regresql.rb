f5f61ff3263b7ce36d35e2c9af5b13bce1eb323d
Radim Marek <radim@boringsql.com>
2025-11-21 00:55:02 +0100

    Update regresql to v1.1.1

--- Formula/regresql.rb
+++ Formula/regresql.rb
@@ -1,17 +1,17 @@
 class Regresql < Formula
   desc "Regression testing framework for SQL queries targeting PostgreSQL databases"
   homepage "https://github.com/boringsql/regresql"
-  url "https://github.com/boringsql/regresql/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "e98f712ac019e9d9a71ffc760be0dc67472ac6a460e04a25c941fca22302be21"
+  url "https://github.com/boringsql/regresql/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "1c73231773e0e83a1486e3d8a47e49a07501705fdf8f27c1d2bbf067eb82519c"
   license "PostgreSQL"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/boringsql/regresql/cmd.version=#{version}")
   end
 
   test do
-    assert_match "regresql version", shell_output("#{bin}/regresql --version 2>&1", 1)
+    assert_match "regresql version", shell_output("#{bin}/regresql --version")
   end
 end
