49c84ea2fce8cf03d83c195a4b82445aefdf515e
Radim Marek <radim@boringsql.com>
2026-02-05 21:44:50 +0100

    fix: alpha4 dependency fix

--- Formula/regresql.rb
+++ Formula/regresql.rb
@@ -2,7 +2,7 @@ class Regresql < Formula
   desc "Regression testing framework for SQL queries targeting PostgreSQL databases"
   homepage "https://github.com/boringsql/regresql"
   url "https://github.com/boringSQL/regresql/archive/refs/tags/v2.0.0-alpha4.tar.gz"
-  sha256 "e3d4c0bd46e409f787917436d306c71896858947bac38b96f04b4bcbffbf618a"
+  sha256 "7727806f28d96700ad2653f168d133adeec02e48c41de7dc23a005ad4593da39"
   license "BSD-2-Clause"
 
   depends_on "go" => :build
