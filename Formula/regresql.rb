933a46843dc141786d945861ba838471c5e10a09
Radim Marek <radim@boringsql.com>
2026-02-05 21:41:15 +0100

    voila v2.0.0-alpha4

--- Formula/regresql.rb
+++ Formula/regresql.rb
@@ -1,8 +1,8 @@
 class Regresql < Formula
   desc "Regression testing framework for SQL queries targeting PostgreSQL databases"
   homepage "https://github.com/boringsql/regresql"
-  url "https://github.com/boringSQL/regresql/archive/refs/tags/v2.0.0-alpha3.tar.gz"
-  sha256 "6a259ae0ebfab56663c53a6b9e63dd7af3c0e50ec6510dc1d684ccb7f85bc546"
+  url "https://github.com/boringSQL/regresql/archive/refs/tags/v2.0.0-alpha4.tar.gz"
+  sha256 "e3d4c0bd46e409f787917436d306c71896858947bac38b96f04b4bcbffbf618a"
   license "BSD-2-Clause"
 
   depends_on "go" => :build
